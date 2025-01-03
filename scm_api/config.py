c468714c1329b3a10168228e497a44a5cd6cb068
木泠 <clemooling@outlook.com>
2025-01-03 14:26:59 +0800

    Update config.py

--- scm_api/config.py
+++ scm_api/config.py
@@ -1,7 +1,12 @@
 from mcdreforged.api.all import *
 
 
-default_config = {"enable_test": False}
+default_config = {
+    "enable": {
+        "test": False,
+        "listen_only": False
+    }
+}
 
 def load_config(server: PluginServerInterface):
     server.logger.info("For developers, please enable test option in config.enable_test, then you can debug.")
