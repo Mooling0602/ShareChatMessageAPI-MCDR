820ab175f4f44dcd1c50fdb0df382f44dee04d3f
木泠 <clemooling@outlook.com>
2025-01-03 14:31:04 +0800

    Update config.py

--- scm_api/config.py
+++ scm_api/config.py
@@ -10,5 +10,5 @@ default_config = {
 
 def load_config(server: PluginServerInterface):
     server.logger.info("For developers, please enable test option in config.enable_test, then you can debug.")
-    config = server.load_config_simple('config', default_config)
+    config = server.load_config_simple('config.json', default_config)
     return config
